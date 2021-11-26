ce62e649f60d690a98f30a9ee3e13c217c054d1b
opilane <opilane@HP-CZC6297BBB>
2021-11-26 13:18:37 +0200

    gg

--- module1.py
+++ module1.py
@@ -1,26 +1,26 @@
 ﻿def adding(palk,inimesed):
-    add=input("Введите имя: ")
+    add=input("vvedite imja: ")
     inimesed.append(add)
-    add_zp=int(input("Введите зарплату: "))
+    add_zp=int(input("vvedite zp: "))
     palk.append(add_zp)
     return palk,inimesed
 def adding(palk,inimesed):
-    add=input("Имя человека:  ")
+    add=input("Imja 4eloveka:  ")
     inimesed.append(add)
-    add_palk=int(input("Зарплата человека:  " ))
+    add_palk=int(input("ZP 4eloveka:  " ))
     palk.append(add_palk)
     return palk,inimesed
     print()
     print()
 def delete(palk, inimesed):
-    x=input("Имя пользователя написано буквами - 1 или цифрами - 2? ")
+    x=input("Imja Polzovatelja napisano bukvami - 1 ILI 2iframi - 2? ")
     if x=="2":
-        i=int(input("Введите номер: "))
+        i=int(input("VVedi nomer: "))
         palk.pop(i-1)
         inimesed.pop(i-1)
     elif x=="1":
         i=0
-        keda=input("Введите имя: ")
+        keda=input("VVedi Imja: ")
         n=len(inimesed)
         while i<n:
             if keda==inimesed[i]:
@@ -39,39 +39,6 @@ def kustutamine():
             inimesed.pop(index)
             print()
             print()
-def sorteerimine(palk,inimesed):
-    abi_p=0
-    abi_i=""
-    n=len(inimesed)
-    for i in range(0,n-1):
-        for j in range(i+1,n):
-            if palk[i]>palk[j]:
-                abi_p=palk[i]
-                palk[i]=palk[j]
-                palk[j]=abi_p
-                abi_i=inimesed[i]
-                inimesed[i]=inimesed[j]
-                inimesed[j]=abi_i
-    return palk,inimesed
-    print()
-    print()
-def nimi(palk,inimesed):
-    ots_nimi=[]
-    ots_palk=[]
-    palk_keda=0
-    keda=input("Введите имя: ")
-    n=len(inimesed)
-    for j in range(n):
-        if inimesed[j]==keda:
-            palk_keda=palk[j]
-            ots_nimi.append(inimesed[j])
-            ots_palk.append(palk_keda)
-            print()
-            print()
-        else:pass
-    return ots_nimi,ots_palk
-    print()
-    print()
 def maksimum(palk,inimesed):
     m_palgad=[]
     nimed=[]
@@ -123,47 +90,3 @@ def keskmine(palk):
     return k
     print()
     print()
-def nimi(palk,inimesed):
-    ots_nimi=[]
-    ots_palk=[]
-    palk_keda=0
-    keda=input("Введите имя: ")
-    n=len(inimesed)
-    for j in range(n):
-        if inimesed[j]==keda:
-            palk_keda=palk[j]
-            ots_nimi.append(inimesed[j])
-            ots_palk.append(palk_keda)
-            print()
-            print()
-        else:pass
-    return ots_nimi,ots_palk
-    print()
-    print()
-def kalk():
-    print("Это калькулятор) Поможет со сложными вычислениями")
-    while True:
-        print("Выберите действие которое хотите сделать:\n"
-              "Сложить: +\n"
-              "Вычесть: -\n"
-              "Умножить: *\n"
-              "Поделить: /\n"
-              "Выйти: q\n")
-        action = input("Действие: ")
-        if action == "q":
-            print("Выход из программы")
-            break
-        if action in ('+', '-', '*', '/'):
-            x = float(input("x = "))
-            y = float(input("y = "))
-            if action == '+':
-                print('%.2f + %.2f = %.2f' % (x, y, x+y))
-            elif action == '-':
-                print('%.2f - %.2f = %.2f' % (x, y, x-y))
-            elif action == '*':
-                print('%.2f * %.2f = %.2f' % (x, y, x*y))
-            elif action == '/':
-                if y != 0:
-                    print('%.2f / %.2f = %.2f' % (x, y, x/y))
-                else:
-                    print("Нельзя на 0 делить")
--- palgad.py
+++ palgad.py
@@ -2,21 +2,21 @@ from module1 import*
 palk=[1200,2500,750,395,1200]
 inimesed=["A","B","C","D","E"]
 def palgad(p,i):
-    valik=input("Средняя зарплата - 1,\nМинимальная зарплата - 2,\nМаксимальная зарплата - 3,\nУдалить человека - 4,\nСортировка - 5, \nДобавить человека - 6\nСвоя функция - 7.\n ")
+    valik=input("srednjaja zarplata - 1,\nminimalka zp - 2,\nMaksimalka zarplata - 3,\nDelete 4eloveka - 4,\nADD 4eloveka - 5")
     if valik=="1":
         kesk_palk=round(keskmine(palk),2)
-        print("Keskmine palk on ",kesk_palk)
+        print("srednjaja ",kesk_palk)
         print()
         print()
     elif valik=="2":
         m_palgad,nimi=minimum(palk,inimesed)
         for n in nimi:
-            print(m_palgad[0], " будет получено ",n)
+            print(m_palgad[0], " budet ",n)
             print()
             print()
     elif valik=="3":
         max_palk,kto=maksimum(palk,inimesed)
-        print("Максимальная зарплата ", max_palk, " будет получено ",kto)
+        print("maksimalnja zp ", max_palk, " budet  ",kto)
         print()
         print()
     elif valik=="4":                                       
@@ -27,20 +27,15 @@ def palgad(p,i):
         else:
             for i in range(len(inimesed)):
                 print(inimesed[i]," saab kätte ", palk[i])
-    elif valik=="5":
-        p,i=sorteerimine(palk,inimesed)
-        for i in range(len(inimesed)):
-            print(inimesed[i]," будет получено ", palk[i])
-            print()
-            print()
-    elif valik=="6":                                        
+    elif valik=="5":                                        
         i=adding(palk,inimesed)
-        print("Обновлённые списки: ")
+        print("Obnovlenie spiski: ")
         print(inimesed)
         print(palk)
-        print("Добавлен элемент", inimesed[-1])
-        print("С зарплатой", str(palk[-1])+'€')
+        print("Dobavlen element", inimesed[-1])
+        print("S zp", str(palk[-1])+'€')
     elif valik=="7":
         x,y=kalk()
 while True:
+
     palgad(palk,inimesed)
